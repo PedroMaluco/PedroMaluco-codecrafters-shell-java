9fa7756afc0a84c6c5633ab4369d41e0fb40a91f
Pedro Maluco <phtbarreto@gmail.com>
2025-11-26 12:43:35 -0300

    Test N1 Step 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+    	while(true) {
         System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         if(command.length() != 0) {
         	System.out.println(command+": command not found");
+        	}
         }
     }
 }
