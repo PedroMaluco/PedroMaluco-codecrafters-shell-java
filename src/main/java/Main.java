089983ac42339bc0191fce42bb1b00e8a05cda48
Pedro Maluco <phtbarreto@gmail.com>
2026-03-19 10:23:04 -0300

    test on double quotes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -196,9 +196,8 @@ public class Main {
 					currentArg = new StringBuilder();
 					
 					if(soloSpace == false) {
-						System.out.println("got into the soloSpace if");
 						soloSpace = !soloSpace;
-						currentArg.append(" ");
+						currentArg.append(' ');
 						
 					}
 				}
